e150b8c591fb8624dddbaee129db6ebc59c46d90
dan1306 <81488150+dan1306@users.noreply.github.com>
2022-11-24 16:52:38 -0500

    working with blockcypher API"

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
-const path = require("path");
-const favicon = require("serve-favicon");
+// const path = require("path");
+// const favicon = require("serve-favicon");
 const logger = require("morgan");
+const bodyParser = require("body-parser");
+const fetch = require("node-fetch");
+
 
 require("dotenv").config();
 require("./config/database");
@@ -10,9 +13,56 @@ const app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.status(200).json("ok it worked")
+
+app.post("/generateAddress", async (req, res) => {
+
+  let data = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+  }
+
+  let url =`https://api.blockcypher.com/v1/btc/test3/addrs?token=${process.env.token}&bech32=true`
+
+  let fetchResponse = await fetch(url, data)
+
+  let data2 = await fetchResponse.json()
+
+  console.log(fetchResponse.ok ,data2)
+
+  res.status(200).json("crea")
+  
+})
+
+
+app.post('/createWallet', async (req, res) => {
+  
+  console.log(req.body)
+
+  let data = {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify(req.body)
+  };
+
+  let fetchResponse =  await fetch(`https://api.blockcypher.com/v1/btc/test3/wallets?token=${process.env.token}`, data);
+
+  let data2 = await fetchResponse.json()
+
+  console.log(fetchResponse.ok ,data2)
+
+  res.status(200).json("cool")
+
+  // let data = {
+  //   method: "POST",
+  //   headers: { "Content-Type": "application/json" },
+  //   body: JSON.stringify({
+      
+  //   }),
+  // };
+
+  
 })
 
 
