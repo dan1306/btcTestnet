5d36288e86d810722efe8b10fba604fd877ce899
dan1306 <81488150+dan1306@users.noreply.github.com>
2022-11-26 15:46:30 -0500

    login logout

--- src/App.js
+++ src/App.js
@@ -1,16 +1,81 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
+import React, { Component } from "react";
+import AuthPage from './pages/AuthPage/AuthPage';
 
 
-function App() {
-  return (
-    <div className="App">
-      <NavBar />
-      <h1>
-        Hi It Is Daniels
-     </h1>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    user: null,
+    showLogin: true,
+
+  };
+
+  componentDidMount = () => {
+    let token = localStorage.getItem("token");
+    if (token) {
+      const payload = JSON.parse(atob(token.split(".")[1]));
+      if (payload.exp < Date.now() / 1000) {
+        localStorage.removeItem("token");
+        token = null;
+      } else {
+        let user = payload.user;
+        this.setState({ user });
+      }
+    }
+  };
+
+  logout = async () => {
+    await this.setState({ showLogin: true });
+
+    localStorage.removeItem("token");
+    let user = null;
+    this.setState({ user });
+  };
+
+  Signup = async () => {
+    await this.setState({ showLogin: false });
+  };
+
+  LogIn = async () => {
+    await this.setState({ showLogin: true });
+  };
+
+
+  setUserInState = (incomingUserData) => {
+    this.setState({ user: incomingUserData });
+  };
+
+
+  render() { 
+    return (
+      <div className="App">
+        <NavBar
+          userState={this.state.user}
+          logout={this.logout}
+          Signup={this.Signup}
+          LogIn={this.LogIn}
+
+        />
+        {
+          this.state.user ? (
+          
+            <h1>
+              You are logged In
+          </h1>
+            
+          ) : (
+            <AuthPage
+            showLogin={this.state.showLogin}
+            setUserInState={this.setUserInState}
+          />
+          )
+        }
+
+      </div>
+    );
+  } 
 }
 
 export default App;
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,38 +1,52 @@
 import { Component } from "react";
-import Container from 'react-bootstrap/Container';
+import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import "./NavBar.css";
 
 export default class NavBar extends Component {
+  render() {
+    const { userState, logout, Signup, LogIn } = this.props;
 
-    render() {
-    
-        return (
-            <Navbar bg="light" expand="lg" className="nav-color">
-              <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                  <Nav className="ms-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#link">Link</Nav.Link>
-                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                      <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                      <NavDropdown.Item href="#action/3.2">
-                        Another action
-                      </NavDropdown.Item>
-                      <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                      <NavDropdown.Divider />
-                      <NavDropdown.Item href="#action/3.4">
-                        Separated link
-                      </NavDropdown.Item>
-                    </NavDropdown>
-                  </Nav>
-                </Navbar.Collapse>
-              </Container>
-            </Navbar>
-          );
-}
+    return (
+      <Navbar bg="light" expand="lg" className="nav-color">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            {userState ? (
+              <Nav className="ms-auto">
+                <Nav.Link
+                  href="#home"
+                  onClick={async () => {
+                    logout();
+                  }}
+                >
+                  LogOut
+                </Nav.Link>
+              </Nav>
+            ) : (
+              <Nav className="ms-auto">
+                <Nav.Link
+                  onClick={async () => {
+                    Signup();
+                  }}
+                >
+                  Signup
+                </Nav.Link>
+                <Nav.Link
+                  onClick={async () => {
+                    LogIn();
+                  }}
+                >
+                  LogIn
+                </Nav.Link>
+              </Nav>
+            )}
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+  }
 }
