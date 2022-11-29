78c582586a64294ff4050994b5cd92f7b381cadd
dan1306 <81488150+dan1306@users.noreply.github.com>
2022-11-29 01:02:20 -0500

    searching backend

--- controllers/search.js
+++ controllers/search.js
@@ -1,6 +1,25 @@
 const fetch = require("node-fetch");
 const Search = require("../models/search");
 
+
+
+let getTimeDiff = (n1, n2, m="m") => {
+    let splitted1 = n1.split(":");
+    let splitted2 = n2.split(":");
+    let time1 = splitted1[0] + splitted1[1];
+    let time2 = splitted2[0] + splitted2[1];
+  
+    if (time1 < time2) {
+      let diff = getTimeDiff(n2, n1, "m");
+      return diff;
+    } else {
+      let diff1 = getTimeDiff("24:00", n1, "m");
+      let diff2 = getTimeDiff(n2, "00:00", "m");
+      let totalDiff = diff1 + diff2;
+      return totalDiff;
+    }
+  };
+
 async function searchAddress(req, res) {
   let addr = req.body.addrs;
 
@@ -8,8 +27,21 @@ async function searchAddress(req, res) {
   console.log(findAddr);
 
   if (findAddr) {
-    console.log(findAddr);
-    return res.status(200).json(findAddr);
+    let tim1;
+    if (findAddr.createdAt.toLocaleTimeString().length === 13) {
+      tim1 = findAddr.createdAt.toLocaleTimeString().slice(0, 5);
+    } else {
+      tim1 = findAddr.createdAt.toLocaleTimeString().slice(0, 4);
+    }
+    let tim2;
+    if (new Date().toLocaleTimeString().length === 13) {
+        tim2 = new Date().toLocaleTimeString().slice(0, 5);
+    } else {
+        tim2 = new Date().toLocaleTimeString().slice(0, 4);
+      }
+      
+    console.log(tim1, tim2);
+    return res.status(200).json("cool");
   } else {
     let fetchResponse = await fetch(
       `https://api.blockcypher.com/v1/btc/test3/addrs/${addr}/full?limit=50`
@@ -29,6 +61,8 @@ async function searchAddress(req, res) {
     }
   }
 }
+
+
 
 module.exports = {
   searchAddress,
